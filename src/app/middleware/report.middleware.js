91e117e4b45fea3a04c901743a48641f6b8b5f64
Truong Viet Huy Phong <17520897@gm.uit.edu.vn>
2019-06-26 09:07:15 +0700

    commit year report feature

--- src/app/middleware/report.middleware.js
+++ src/app/middleware/report.middleware.js
@@ -5,13 +5,30 @@ module.exports.postDownloadMonthReport = async(req, res, next) => {
     try {
         let downloadReportMonth = req.body.downloadReportMonth;
         let flights = await flightModel.find({ 'flightDate.month': parseInt(downloadReportMonth) });
-        console.log(flights);
         if (downloadReportMonth === '') {
             req.flash('error', 'Chưa có thống kê hiển thị để xuất báo cáo');
             res.redirect('/report/month');
         } else if (flights.length <= 0) {
             req.flash('error', 'Không có dữ liệu để xuất báo cáo');
             res.redirect('/report/month');
+        } else {
+            next();
+        }
+    } catch (err) {
+        res.status(500);
+        res.send("Lỗi server hãy tải lại trang");
+        console.log(err);
+    }
+}
+
+module.exports.postDownloadYear = async(req, res, next) => {
+    try {
+        let downloadReportYear = req.body.downloadReportYear;
+        let flights = await flightModel.find({ 'flightDate.year': parseInt(downloadReportYear) });
+        if (flights.length <= 0) {
+            req.flash('error', 'Không có dữ liệu trong thống kê để xuất báo cáo');
+            res.redirect('/report/year');
+            return;
         } else {
             next();
         }
--- src/app/routers/report/report.js
+++ src/app/routers/report/report.js
@@ -13,10 +13,7 @@ const router = express.Router();
 
 router.get('/month', reportController.getMonthReport);
 
-router.get('/year', reportController.getYearReport);
-
 router.post('/month', reportController.postMonthReport);
-
 
 router.post('/month/download', reportMiddleware.postDownloadMonthReport, async(req, res) => {
     let downloadReportMonth = req.body.downloadReportMonth;
@@ -33,13 +30,35 @@ router.post('/month/download', reportMiddleware.postDownloadMonthReport, async(r
         };
         report.push(item);
     }
-    let test = await new objectToCSV(report).toString();
+    let csv = await new objectToCSV(report).toString();
     let datetime = moment().format('ssmmhhDDMMYYYY');
     let file = path.join(__dirname, '..', '..', 'public', 'report', 'month', "month-" + downloadReportMonth + "-report-" + datetime + ".csv");
-    fs.writeFileSync(file, test)
+    fs.writeFileSync(file, csv)
     res.download(file);
 });
 
+router.get('/year', reportController.getYearReport);
 
+router.post('/year', reportController.postYearReport);
+
+router.post('/year/download', reportMiddleware.postDownloadYear, async(req, res) => {
+    let downloadReportYear = req.body.downloadReportYear;
+    let report = [];
+    let flights = await reportService.yearReport(downloadReportYear);
+    for (flight of flights) {
+        let item = {
+            Thang: flight.month,
+            SoChuyenBay: flight.numberOfFlight,
+            DoanhThu: flight.monthIncome,
+            TyLe: flight.ratio,
+        };
+        report.push(item);
+    }
+    let csv = await new objectToCSV(report).toString();
+    let datetime = moment().format('ssmmhhDDMMYYYY');
+    let file = path.join(__dirname, '..', '..', 'public', 'report', 'year', "year-" + downloadReportYear + "-report-" + datetime + ".csv");
+    fs.writeFileSync(file, csv)
+    res.download(file);
+});
 
 module.exports = router;
